fb9ce16b7a6aeb821285d4f6d9e5f29258db5c0e
matheus <matheus-pxt@hotmail.com>
2019-11-14 22:54:53 -0300

    CRUD's done

--- src/main/java/br/com/matheuspeixoto/apimongodb/service/UserService.java
+++ src/main/java/br/com/matheuspeixoto/apimongodb/service/UserService.java
@@ -1,5 +1,6 @@
 package br.com.matheuspeixoto.apimongodb.service;
 
+import br.com.matheuspeixoto.apimongodb.dto.UserDTO;
 import br.com.matheuspeixoto.apimongodb.repository.UserRepository;
 import br.com.matheuspeixoto.apimongodb.service.exception.ObjectNotFoundException;
 import br.com.matheuspeixoto.apimongodb.web.domain.User;
@@ -21,5 +22,33 @@ public class UserService {
 
     public User findById(String id) {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return userRepository.save(newUser);
+    }
+
+    private void updateData(User newUser, User user) {
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return User.builder()
+                .id(userDTO.getId())
+                .name(userDTO.getName())
+                .email(userDTO.getEmail())
+                .build();
     }
 }
--- src/main/java/br/com/matheuspeixoto/apimongodb/web/resources/UserResources.java
+++ src/main/java/br/com/matheuspeixoto/apimongodb/web/resources/UserResources.java
@@ -5,12 +5,11 @@ import br.com.matheuspeixoto.apimongodb.web.domain.User;
 import br.com.matheuspeixoto.apimongodb.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.websocket.server.PathParam;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,5 +33,27 @@ public class UserResources {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         UserDTO userDTO = new UserDTO(userService.findById(id));
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+        User user = userService.fromDTO(userDTO);
+        user.setId(id);
+        userService.update(user);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
